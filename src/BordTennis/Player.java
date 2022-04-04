9a9e197c01cac828bb7345906a5e4cd867938a76
Misfosster <williamfossnielsen@hotmail.com>
2022-04-04 12:48:00 +0200

    Tilføjede til development i stedet for main

--- src/BordTennis/Player.java
+++ src/BordTennis/Player.java
@@ -1,4 +1,28 @@
 package BordTennis;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Player {
+
+    Scanner sc = new Scanner(System.in);
+    String playerName;
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    ArrayList<Player> players = new ArrayList<>();
+
+    public void createPlayer(ArrayList<Player> arrayList) {
+        String input = sc.nextLine();
+        System.out.println("What's your name?\n");
+        Player player = new Player(input);
+        arrayList.add(player);
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + playerName;
+    }
 }
