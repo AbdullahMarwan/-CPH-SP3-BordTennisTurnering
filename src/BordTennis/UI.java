b02d2b31313c138d728488a204a965b24cb0d9d1
Emilio Castro Lagunas <EmilioCLDK@gmail.com>
2022-04-04 12:41:11 +0200

    Added UI methods

--- src/BordTennis/UI.java
+++ src/BordTennis/UI.java
@@ -1,4 +1,23 @@
 package BordTennis;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class UI {
+
+    Scanner sc = new Scanner(System.in);
+
+    private ArrayList<Team> teamList = new ArrayList<>();
+
+    private String getUserInput(String msg) {
+        return null;
+    }
+
+    private void Options() {
+
+    }
+
+    public void inputFromUser(){
+
+    }
 }
