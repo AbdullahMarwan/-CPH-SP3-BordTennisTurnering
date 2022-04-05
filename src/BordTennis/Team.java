4b3e6789e4e0263c57cc463568756f7aaadea8f4
AbdullahMarwan <abdu.moussa01@gmail.com>
2022-04-05 12:15:20 +0200

    Formatting issues fixed

--- src/BordTennis/Team.java
+++ src/BordTennis/Team.java
@@ -3,45 +3,32 @@ package BordTennis;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Team
-{
+public class Team {
     Scanner sc = new Scanner(System.in);
     String teamName;
     ArrayList<Player> team;
     Player player;
 
-    public Team(String teamName)
-    {
-<<<<<<< Updated upstream
-        this.teamName=teamName;
-=======
+    public Team(String teamName) {
         this.teamName = teamName;
->>>>>>> Stashed changes
     }
 
-    public void createTeam()
-    {
+    public void createTeam() {
         System.out.println("What's your team name?\n");
         String teamName = sc.nextLine();
-        this.teamName=teamName;
+        this.teamName = teamName;
         System.out.println("How many players are on your team?\n");
         int teamSize = sc.nextInt();
-        try
-        {
-            for (int i = Math.abs(teamSize-5); i<5 && i>=2; i++)
-            {
+        try {
+            for (int i = Math.abs(teamSize - 5); i < 5 && i >= 2; i++) {
                 player.createPlayer(team);
             }
-        }
-
-        catch (IndexOutOfBoundsException e)
-        {
+        } catch (IndexOutOfBoundsException e) {
             System.out.println("Invalid team size - must be between 2-5.");
         }
     }
 
-    public String getTeamName() 
-    {
+    public String getTeamName() {
         return teamName;
     }
 }
