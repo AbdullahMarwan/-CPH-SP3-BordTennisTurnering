7c3fafd070a8aa56fae172c23e1462724bab4fc2
Misfosster <williamfossnielsen@hotmail.com>
2022-04-05 14:05:08 +0200

    Teamklassen lavet og prøver at køre den i main.

--- src/BordTennis/Main.java
+++ src/BordTennis/Main.java
@@ -2,7 +2,10 @@ package BordTennis;
 
 public class Main {
 
-    public static void main(String[] args) {
-	
+
+    public static void main(String[] args)
+    {
+        Team team = null;
+        team.createTeam();
     }
 }
--- src/BordTennis/Team.java
+++ src/BordTennis/Team.java
@@ -9,19 +9,22 @@ public class Team {
     ArrayList<Player> team;
     Player player;
 
-    public Team(String teamName) {
+    public Team(String teamName, ArrayList<Player> team) {
         this.teamName = teamName;
+        this.team = team;
     }
 
-    public void createTeam(ArrayList<Player> team) {
+    public void createTeam() {
+        ArrayList<Player> teamArray = new ArrayList<>();
         System.out.println("What's your team name?\n");
         String teamName = sc.nextLine();
-        System.out.println("How many players are on your team?\n");
+        System.out.println("What's the team size?\n");
         int teamSize = sc.nextInt();
         try {
             for (int i = Math.abs(teamSize - 5); i < 5 && i >= 2; i++) {
-                player.createPlayer(team);
+                player.createPlayer(teamArray);
             }
+            Team team = new Team(teamName, teamArray);
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Invalid team size - must be between 2-5.");
         }
