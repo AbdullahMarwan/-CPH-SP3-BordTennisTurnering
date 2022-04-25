9f8115a66cb6ab81484f6c658823ce568be95bf6
Emilio Lagunas <Cph-ec100@cphbusiness.dk>
2022-04-25 09:58:26 +0200

    Added DBConnector

---
+++ src/BordTennis/DBConnector.java
@@ -0,0 +1,8 @@
+package BordTennis;
+
+public class DBConnector {
+
+    String JdbcUrl = "jdbc:mysql://localhost/Bordfodbold";
+    String population = "root";
+    String password = "NY9hcW7DAZ!+-qg1:]G6";
+}
